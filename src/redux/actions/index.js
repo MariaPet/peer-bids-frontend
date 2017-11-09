aa6313c0490a5ac76457d9d9a3ba72e04945b80e
mariapetriti <petriti.maria@gmail.com>
2017-11-09 03:07:30 +0200

    remove comment with old backend

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,6 @@ import $ from 'jquery'
 export const getApiData = (dispatch, state) => {
     dispatch({type: 'DATA_LOADING'});
     var flaskAPI = "https://peer-bids-back-end.appspot.com/api/dummy"
-    // var flaskAPI = "https://mariapet-182810.appspot.com/api/dummy"
     // var flaskAPI = "http://localhost:5000/api/dummy";
     $.getJSON(flaskAPI).done(data => {
         console.log(data);

c0cfcb21ceeed6cbcd0f78974fc3017c8789957d
mariapetriti <petriti.maria@gmail.com>
2017-12-14 04:38:02 +0200

    Login Register components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,12 +15,30 @@ import {
     export default class Navigation extends Component {
         constructor(props) {
             super(props);
+            this.toggle = this.toggle.bind(this);
+            this.state = {
+                isOpen: false
+            };
+        }
+        toggle() {
+            this.setState({
+                isOpen: !this.state.isOpen
+            });
         }
         render() {
             return (
                 <div>
-                    <Navbar color="primary" light expand="md">
+                    <Navbar color="primary" dark expand="md">
                         <NavbarBrand href="/">Peer Bids</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="#">Login</NavLink>
+                                    <NavLink href="#">Register</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </Navbar>
                 </div> 
             )
--- src/components/Test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Test() {
-    return (
-        <div>
-            <h2>Test Route</h2>
-        </div>
-    )
-}

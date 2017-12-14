1514c22ad2d92cf03ddb7c61fc9aa92e5a640e6e
kholoudsaa <kholoudsaa@gmail.com>
2017-12-14 15:24:36 +0000

    Add search box

--- src/components/FrontSearch.js
+++ src/components/FrontSearch.js
@@ -7,8 +7,17 @@ export default class FrontSearch extends Component {
     render() {
         return (
             <div>
-                <div>
-                    Search is coming soon!
+                <div className="col-sm-12 col-sm-order-2 col-sm-offset-3 d-flex align-items-center justify-content-center" style={{height:'700px'}}>
+                    <div className="jumbotron">
+                        <h1 className="display-4">Find your item</h1>
+                        <p className="lead d-flex justify-content-center">Search Auctions in the Southampton </p>
+                        <p className="lead d-flex justify-content-center"> you may find what you are looking for! </p>
+                        <hr className="my-4"/>
+                        <div className="d-flex justify-content-center">
+                            <input type="text"  placeholder="Search" />
+                            <button type="submit" className="btn btn-outline-primary"> Search  </button> 
+                        </div> 
+                    </div>
                 </div>
             </div>
         );

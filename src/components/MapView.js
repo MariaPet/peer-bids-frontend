139991123a49e849819fc854880bce3fb842dfe3
mariapetriti <petriti.maria@gmail.com>
2017-12-15 17:01:43 +0200

    map with marker not clickable

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react'
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from 'google-map-react'
+import markerIcon from '../img/marker.png'
+import {Popover, PopoverHeader, PopoverBody} from 'reactstrap'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <img id="imgPopover" style={{height: "30px", width: "30px"}} src={markerIcon}/>;
 
 export default class MapView extends Component {
     constructor(props) {
         super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          popoverOpen: false
+        };
     }
-
     static defaultProps = {
         center: {lat: 50.9, lng: -1.4},
         zoom: 13
-      };
+    };
+    
+    
+    toggle() {
+        this.setState({
+            popoverOpen: !this.state.popoverOpen
+        })
+    }
 
     render() {
         return (
@@ -23,11 +35,29 @@ export default class MapView extends Component {
                 defaultZoom={this.props.zoom}
             >
                 <AnyReactComponent
-                lat={59.955413}
-                lng={30.337844}
-                text={'Kreyser Avrora'}
+                lat={50.9}
+                lng={-1.4}
                 />
+                <Popover placement="bottom" isOpen={this.state.popoverOpen} target="imgPopover" toggle={this.toggle}>
+                    <PopoverHeader>Popover Title</PopoverHeader>
+                    <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+                </Popover>
             </GoogleMapReact>
         );
+    }
+}
+
+class AuctionPreview extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+                <PopoverHeader>Popover Title</PopoverHeader>
+                <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+            </Popover>
+        )
     }
 }

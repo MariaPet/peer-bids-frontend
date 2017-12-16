1d7fc0345eb1b7daeca0d0ee35077a101180d731
mariapetriti <petriti.maria@gmail.com>
2017-12-16 03:02:27 +0200

    basic slider styling

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react'
-import markerIcon from '../img/marker.png'
-import {Popover, PopoverHeader, PopoverBody} from 'reactstrap'
-
-const AnyReactComponent = ({ text }) => <img id="imgPopover" style={{height: "30px", width: "30px"}} src={markerIcon}/>;
+import '../styles/mapview.css'
+import {Row, Col} from 'reactstrap'
+import $ from 'jquery'; 
 
 export default class MapView extends Component {
     componentDidMount() {
+        //Initialize map
         let map = new window.google.maps.Map(document.getElementById('map'),
         {
             center: {lat: 50.9, lng: -1.4},
             zoom: 13
         });
+        //Initialize markers
+        let marker = new window.google.maps.Marker({
+            map,
+            position: {lat: 50.9, lng: -1.4}
+        });
+        marker.addListener('click', () => {
+            //Open slider with info
+            $('.transform-slider').toggleClass('slide-in');
+        });
     }
 
     constructor(props) {
         super(props);
+        this.state = {}
     }
 
     render() {
         return (
-            <div>
-                <div id="map" style={{height: "400px"}} />
+            <div id="mapView">
+                <div id="map" />
+                <div id="auctionPreview" className="transform-slider">
+                </div>
             </div>
         );
     }
@@ -33,10 +45,8 @@ class AuctionPreview extends Component {
     
     render() {
         return (
-            <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
-                <PopoverHeader>Popover Title</PopoverHeader>
-                <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
-            </Popover>
+            <div style={{height: "400px", width: "30%", background: "blue", position:"absolute", bottom: "-100%"}}>
+            </div>
         )
     }
 }

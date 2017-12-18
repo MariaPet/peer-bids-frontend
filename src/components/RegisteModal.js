118ae0fe262b6e1435ce7c068c9c7de3db4d94fd
mariapetriti <petriti.maria@gmail.com>
2017-12-18 19:24:57 +0200

    Register form inputs and layout

--- src/components/RegisteModal.js
+++ src/components/RegisteModal.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,
+Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
 
 export default class RegisterModal extends Component {
     constructor(props) {
@@ -11,13 +12,75 @@ export default class RegisterModal extends Component {
             <Modal isOpen={this.props.modal} toggle={this.props.toggleModal} className={this.props.className}>
                 <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <RegisterForm />
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={this.props.toggleModal}>Do Something</Button>{' '}
                     <Button color="secondary" onClick={this.props.toggleModal}>Cancel</Button>
                 </ModalFooter>
             </Modal>
+        );
+    }
+}
+
+class RegisterForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Form>
+                <Row>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="username">Username</Label>
+                            <Input name="username" id="username" placeholder="Enter a username" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="email">Email</Label>
+                            <Input type="email" name="email" id="email" placeholder="Enter your email address" />
+                        </FormGroup>
+                        </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="password">Password</Label>
+                            <Input type="password" name="password" id="password" placeholder="Enter a password" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="confirmPassword">Confirm Password</Label>
+                            <Input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm your password" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="street">Address</Label>
+                            <Input name="street" id="street" placeholder="Enter your address number and street name" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="city">City</Label>
+                            <Input name="city" id="city" placeholder="Enter your city" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="postalCode">Postal Code</Label>
+                            <Input name="postalCode" id="postalCode" placeholder="Enter your postal code" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="6">
+                        <FormGroup>
+                            <Label for="telephone">Phone Number</Label>
+                            <Input name="telephone" id="telephone" placeholder="Enter your phone number" />
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </Form>
         );
     }
 }

708b562dbfe67459e0d5bf8e780290adba3c9ee8
lakshmimukkawar <32481307+lakshmimukkawar@users.noreply.github.com>
2017-12-20 09:04:58 +0000

    adding attributes in the auction

--- src/components/NewAuctionForm.js
+++ src/components/NewAuctionForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter,
-Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
+Form, FormGroup, Label, InputGroup, InputGroupAddon, Input } from 'reactstrap';
 
 export default class NewAuctionForm extends Component {
     constructor(props) {
@@ -8,7 +8,10 @@ export default class NewAuctionForm extends Component {
         this.state = {
             ItemName: "",
             description: "",
-            minPrice: ""
+            minPrice: "",
+            status: "",
+            expiryDate: "",
+            category: ""
         };
         this.onInput = this.onInput.bind(this);
     }
@@ -32,17 +35,43 @@ export default class NewAuctionForm extends Component {
             </FormGroup>
             <FormGroup>
                 <Label for="description">Description</Label>
-                <Input name="description" id="description" placeholder="Enter description" value={this.state.description} 
+                <Input type="textarea" name="description" id="description" placeholder="Enter description" value={this.state.description} 
                 onChange={this.onInput} />
             </FormGroup>
             <FormGroup>
+                <Label for="status">Status</Label>
+                <Input name="status" id="status" placeholder="Enter Status" value={this.state.status} 
+                onChange={this.onInput} />
+            </FormGroup>
+            {/* <FormGroup>
                 <Label for="minPrice"> Minimum Price</Label>
                 <Input name="minPrice" id="minPrice" placeholder="Enter a minimum price" value={this.state.minPrice} 
                 onChange={this.onInput} />
+            </FormGroup> */}
+            <FormGroup>
+                <Label for="expiryDate">Expiration Date</Label>
+                <Input type="date" name="expiryDate" id="expiryDate" placeholder="Enter Expireation Date" value={this.state.expiryDate} 
+                onChange={this.onInput}/>
             </FormGroup>
             <FormGroup>
-                <Button color="primary" type = "submit" onClick={this.props.createAuction(this.state)}>Create Auction</Button>
+                <Label for="category">Category</Label>
+                <Input type="select" name="category" id="category" placeholder="Enter category">
+                <option>Default Select</option>
+                <option>Default Select1</option>
+                <option>Default Select2</option>
+                </Input>
             </FormGroup>
+            <FormGroup>
+            <Label for="minPrice">Minimum Price</Label>
+            <InputGroup>
+                <InputGroupAddon>$</InputGroupAddon>
+                <Input  type="number" name = "minPrice" id = "minPrice" placeholder="Enter minimum price" step="1" value={this.state.minPrice} 
+                onChange={this.onInput}/>
+            </InputGroup>
+            </FormGroup>
+            <Button color="primary" type = "submit" onClick={this.props.createAuction(this.state)}>Create Auction</Button>
+          
         </Form>);
     }
 }
+// £

7e8fe237c8b651f729dea6930170d429ee20dae1
mariapetriti <petriti.maria@gmail.com>
2017-12-23 02:55:56 +0200

    register modal autocomplete address

--- src/components/RegisterModal.js
+++ src/components/RegisterModal.js
@@ -1,38 +1,83 @@
 import React, {Component} from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter,
 Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 export default class RegisterModal extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            username: "",
-            email: "",
-            password: "",
-            confirm_password: "",
-            street: "",
-            city: "",
-            postal_code: "",
-            telephone: ""
+            inputs: {
+                username: "",
+                email: "",
+                password: "",
+                confirm_password: "",
+                street: "",
+                streetNo: "",
+                city: "",
+                postal_code: "",
+                telephone: ""
+            },
+            searchInput: ""
         }
         this.onInput = this.onInput.bind(this);
+        this.onAddressInput = this.onAddressInput.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
     }
     onInput = e => {
         let key = e.target.name;
         let value = e.target.value;
-        let newState = {};
-        newState[key] = value;
-        this.setState(newState);
+        var newInputs = {...this.state.inputs};
+        newInputs[key] = value;
+        this.setState({inputs: newInputs});
+    }
+    onAddressInput = address => {
+        this.setState({searchInput: address});
+    }
+    handleSelect = (address, placeId) => {
+        geocodeByAddress(address)
+        .then(results => {
+            Array.prototype.forEach.call(results[0].address_components, element => {
+                if (element.types.includes("postal_code")) {
+                    var newInputs = {...this.state.inputs};
+                    newInputs.postal_code = element.long_name
+                    this.setState({inputs: newInputs});
+                }
+                else if (element.types.includes("postal_town")) {
+                    var newInputs = {...this.state.inputs};
+                    newInputs.city = element.long_name;
+                    this.setState({inputs: newInputs});
+                }
+                else if (element.types.includes("route")) {
+                    var newInputs = {...this.state.inputs};
+                    newInputs.street = element.long_name;
+                    this.setState({inputs: newInputs});
+                }
+                else if (element.types.includes("street_number")) {
+                    var newInputs = {...this.state.inputs};
+                    newInputs.streetNo = element.long_name;
+                    this.setState({inputs: newInputs});
+                }
+            });
+            var newInputs = {...this.state.inputs};
+            newInputs.street = newInputs.streetNo + " " + newInputs.street;
+            this.setState({inputs: newInputs});
+        })
+        .then(latLng => console.log('Success', latLng))
+        .catch(error => console.error('Error', error))
     }
     render() {
         return (
             <Modal isOpen={this.props.modal} toggle={this.props.toggleModal} className={this.props.className}>
                 <ModalHeader toggle={this.toggle}>REGISTER</ModalHeader>
                 <ModalBody>
-                    <RegisterForm onInput={this.onInput} inputs={this.state}/>
+                    <RegisterForm onInput={this.onInput} 
+                    onAddressInput={this.onAddressInput} 
+                    handleSelect={this.handleSelect} inputs={this.state.inputs} searchInput={this.state.searchInput}/>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" disabled={this.props.loading} onClick={(e) => this.props.registerUser(this.state)}>Register</Button>{' '}
+                    <Button color="primary" disabled={this.props.loading} onClick={(e) => this.props.registerUser(this.state.inputs)}>Register</Button>{' '}
                     <Button color="secondary" disabled={this.props.loading} onClick={this.props.toggleModal}>Cancel</Button>
                 </ModalFooter>
             </Modal>
@@ -79,25 +124,32 @@ class RegisterForm extends Component {
                             onChange={this.props.onInput} />
                         </FormGroup>
                     </Col>
+                    <Col sm="12" md="12">
+                        <FormGroup>
+                            <Label for="searchInput">Search Address</Label>
+                            <PlacesAutocomplete inputProps={{value: this.props.searchInput,
+                                onChange: this.props.onAddressInput, 
+                                placeholder: "Search your address"}}
+                                classNames={{autocompleteContainer: "pac-container"}}
+                                onSelect={this.props.handleSelect} name="searchInput" id="searchInput" />
+                        </FormGroup>
+                    </Col>
                     <Col sm="12" md="6">
                         <FormGroup>
-                            <Label for="street">Address</Label>
-                            <Input name="street" id="street" placeholder="Enter your address number and street name" value={this.props.inputs.street} 
-                            onChange={this.props.onInput} />
+                            <Label for="street">Street</Label>
+                            <Input disabled name="street" id="street" value={this.props.inputs.street} />
                         </FormGroup>
                     </Col>
                     <Col sm="12" md="6">
                         <FormGroup>
                             <Label for="city">City</Label>
-                            <Input name="city" id="city" placeholder="Enter your city" value={this.props.inputs.city} 
-                            onChange={this.props.onInput} />
+                            <Input disabled name="city" id="city" value={this.props.inputs.city} />
                         </FormGroup>
                     </Col>
                     <Col sm="12" md="6">
                         <FormGroup>
                             <Label for="postal_code">Postal Code</Label>
-                            <Input name="postal_code" id="postal_code" placeholder="Enter your postal code" value={this.props.inputs.postal_code} 
-                            onChange={this.props.onInput} />
+                            <Input disabled name="postal_code" id="postal_code" value={this.props.inputs.postal_code} />
                         </FormGroup>
                     </Col>
                     <Col sm="12" md="6">

49e6a7e9f38cce6b2f2f98893d73235fe83c797e
coltaemanuela <coltaemanuela@gmail.com>
2017-12-28 15:43:09 +0000

    set recognition language to English

--- src/components/RealtimeBid.js
+++ src/components/RealtimeBid.js
@@ -1,41 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import SpeechRecognition from 'react-speech-recognition'
-import { Button,Form, FormGroup, Label, InputGroup, InputGroupAddon, Input, Row, Col } from 'reactstrap';
+import { Button, Form, Input} from 'reactstrap';
 
 const propTypes = {
-    // Props injected by SpeechRecognition
     transcript: PropTypes.string,
     resetTranscript: PropTypes.func,
-    browserSupportsSpeechRecognition: PropTypes.bool
+    browserSupportsSpeechRecognition: PropTypes.bool,
+    recognition: PropTypes.string
 }
-          
- class RealtimeBid extends Component {
+
+class RealtimeBid extends Component {
     constructor(props) {
         super(props);
         this.state = {
             bid_value: ""
         }
-        // this.onInput = this.onInput.bind(this);
-    }
-    // onInput = e => {
-    //     let key = e.target.name;
-    //     let value = e.target.value;
-    //     let newState = {};
-    //     newState[key] = value;
-    //     this.setState(newState);
-    // }
+    }   
     render() {
-        const { transcript, resetTranscript, browserSupportsSpeechRecognition } = this.props
-    
+        const { transcript, resetTranscript, browserSupportsSpeechRecognition, recognition } = this.props
+        recognition.lang = 'en-US'
+        recognition.interimResults = false;
         if (!browserSupportsSpeechRecognition) {
         return null
-        }
-    
+        }    
         return (
-        <div>
+        <div>            
             <button onClick={resetTranscript}>Reset</button>
-            <span>{transcript}</span>
+            <Form name="form">
+                <Input type="text" name="bid_value" id="bid_value" value= {transcript} />
+            </Form>           
         </div>
         )
     }   
